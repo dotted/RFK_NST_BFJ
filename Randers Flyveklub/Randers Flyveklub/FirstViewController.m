85db7171caf7ddffd11b863b56a6e4a022dcfeea
Bo Fjord Jenen <bo@bxd.dk>
2013-02-19 12:42:06 +0100

    Initial prject files

---
+++ Randers Flyveklub/Randers Flyveklub/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  Randers Flyveklub
+//
+//  Created by Mercantec on 2/19/13.
+//  Copyright (c) 2013 Nestea & Bo Fucking Jensen. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
