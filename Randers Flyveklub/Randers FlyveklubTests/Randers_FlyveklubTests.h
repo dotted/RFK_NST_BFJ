2d2bdf933e5d058d233edc9d998ce446503f8aaf
Bo Fjord Jenen <bo@bxd.dk>
2013-02-19 12:42:30 +0100

    FU

---
+++ Randers Flyveklub/Randers FlyveklubTests/Randers_FlyveklubTests.h
@@ -0,0 +1,13 @@
+//
+//  Randers_FlyveklubTests.h
+//  Randers FlyveklubTests
+//
+//  Created by Mercantec on 2/19/13.
+//  Copyright (c) 2013 Nestea & Bo Fucking Jensen. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Randers_FlyveklubTests : SenTestCase
+
+@end
---
+++ Randers Flyveklub/Randers FlyveklubTests/Randers_FlyveklubTests.m
@@ -0,0 +1,32 @@
+//
+//  Randers_FlyveklubTests.m
+//  Randers FlyveklubTests
+//
+//  Created by Mercantec on 2/19/13.
+//  Copyright (c) 2013 Nestea & Bo Fucking Jensen. All rights reserved.
+//
+
+#import "Randers_FlyveklubTests.h"
+
+@implementation Randers_FlyveklubTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Randers FlyveklubTests");
+}
+
+@end
